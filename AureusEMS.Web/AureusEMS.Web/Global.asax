f8592cb8de72b840ddcbe75f6f5632ba6120b9fd
adhikarishiva <adhikari.shiva1970@gmail.com>
2017-02-16 11:52:26 -0700

    Project Initilization

---
+++ AureusEMS.Web/AureusEMS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AureusEMS.Web.MvcApplication" Language="C#" %>
---
+++ AureusEMS.Web/AureusEMS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AureusEMS.Web.Startup))]
+namespace AureusEMS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
